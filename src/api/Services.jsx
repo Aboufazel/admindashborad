3b048fab3aac5b0e7e2f319a3383132d054ad3cd
Aboufazel <a.ab2521@gmail.com>
2023-01-07 00:19:30 +0330

    Ref : add one services for user and add header config

--- src/api/Services.jsx
+++ src/api/Services.jsx
@@ -1,22 +1,42 @@
 
 import axios from "axios";
-import Storage from "../Service/Storage";
-const st = Storage();
+
 
 const Api = axios.create({
     baseURL:"http://siavashma.ir"
 })
 
+const userAccessData = localStorage.getItem("auth");
+
+// for setup header data from localStorage
+
+const config = {
+    headers:{
+        selfUserId: userAccessData.userId,
+        token:userAccessData.token,
+    }
+}
 
 Api.interceptors.response.use()
+
+
+//use for user login
 
 export const LoginApi = (username , password)=>{
 
     return Api.get(`/UserService/api/Users/getbyusernamepassword?username=${username}&password=${password}`);
 }
 
-export const GetFromUser = (userId , token)=>{
-    return Api.get(``)
+
+
+
+export const GetAllFromUser = ()=>{
+    return Api.get(`/UserService/api/Users/getall` , {config})
+}
+
+
+export const GetById = (idCode)=>{
+    return Api.get(`/UserService/api/Users/getbyid?useid=${idCode}` , {config})
 }
 
 export default Api;
