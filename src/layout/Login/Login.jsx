08dab94edacec0e5b04ae779c0cc4a99f1e17a80
Aboufazel <a.ab2521@gmail.com>
2023-02-03 23:36:54 +0330

    Ref : login page refactor

--- src/layout/Login/Login.jsx
+++ src/layout/Login/Login.jsx
@@ -4,11 +4,12 @@ import {LoginApi} from "../../api/Services"
 import {useNavigate} from "react-router-dom";
 import useStorage from "../../hooks/storage";
 import {useState} from "react";
+import LoginVector from "../../assets/pics/loginVector.png"
 
 
 const Login = ({}) => {
-const [showAlert , setShowAlert] = useState(false);
-const [message , setMessage] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [message, setMessage] = useState(false);
     const navigate = useNavigate();
     const [authInfo, setAuthInfo] = useStorage("auth", {
         userId: "",
@@ -25,54 +26,61 @@ const [message , setMessage] = useState(false);
 
     const manageSubmit = (e) => {
         e.preventDefault();
-        LoginApi(state.email , state.password)
+        LoginApi(state.email, state.password)
             .then(res => {
-                if (res.data.isSuccess === true){
+                if (res.data.isSuccess === true) {
                     setAuthInfo({
                         userId: res.data.data.userId,
                         accessToken: res.data.data.token,
                         isLogin: true,
                     });
                     navigate("/");
-                }
-                else {
+                } else {
                     setMessage(res.data.message);
                     setShowAlert(true);
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         setShowAlert(false)
-                    } , 1000)
+                    }, 1000)
                 }
             })
 
     };
     return (
         <Container className={"d-flex justify-content-center align-items-center"}>
-            <Alert style={{position:"absolute" , top:25 , left:25 , fontFamily:'iran-sans'}} variant={"danger"} show={showAlert}>
+            <Alert style={{position: "absolute", top: 25, left: 25, fontFamily: 'iran-sans'}} variant={"danger"}
+                   show={showAlert}>
                 {message}
             </Alert>
-            <Row className={"my-auto vh-100"}>
-                <Col className={"my-auto w-75 d-flex justify-content-center"}>
-                    <div className={"login-box"}>
-                        <Form onSubmit={manageSubmit} className={"d-flex flex-column w-auto"}>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>{"نام کاربری یا شماره موبایل"}</Form.Label>
-                                <Form.Control  onChange={(e) => setState({ ...state, email: e.target.value })}
-                                                          value={state.email} type="text" placeholder="نام کاربری"/>
-                            </Form.Group>
+            <Row className={"my-auto vh-100 d-flex align-items-center justify-content-center"}>
+                <div className={"login-block"}>
+                    <Row className={"d-flex justify-content-center align-items-center"}>
+                        <Col className={"my-auto w-75 d-flex justify-content-center"}>
+                            <img src={LoginVector} alt={"ورود به پنل"}/>
+                        </Col>
+                        <Col className={"my-auto w-75 d-flex justify-content-center"}>
+                            <div className={"login-box"}>
+                                <Form onSubmit={manageSubmit} className={"d-flex flex-column w-auto"}>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>{"نام کاربری یا شماره موبایل"}</Form.Label>
+                                        <Form.Control onChange={(e) => setState({...state, email: e.target.value})}
+                                                      value={state.email} type="text" placeholder="نام کاربری"/>
+                                    </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>{"رمز عبور"}</Form.Label>
-                                <Form.Control onChange={(e) => setState({ ...state, password: e.target.value })}
-                                                         value={state.password} type="password" placeholder="رمز عبور"/>
-                            </Form.Group>
-                            <Row className={"d-flex mt-4 justify-content-center "}>
-                                <Button className={"w-50 button-primary"} variant="primary" type="submit">
-                                    {"ورود"}
-                                </Button>
-                            </Row>
-                        </Form>
-                    </div>
-                </Col>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label>{"رمز عبور"}</Form.Label>
+                                        <Form.Control onChange={(e) => setState({...state, password: e.target.value})}
+                                                      value={state.password} type="password" placeholder="رمز عبور"/>
+                                    </Form.Group>
+                                    <Row className={"d-flex mt-4 justify-content-center "}>
+                                        <Button className={"w-50 button-primary"} variant="primary" type="submit">
+                                            {"ورود"}
+                                        </Button>
+                                    </Row>
+                                </Form>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </Row>
         </Container>
     )
