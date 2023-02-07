6fa6da9759a2a29c91a55873fc905650dd7c242c
Aboufazel <a.ab2521@gmail.com>
2023-02-07 23:29:22 +0330

    Feat : account Type Complete

--- src/api/AccountType.jsx
+++ src/api/AccountType.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import AccountGroup from "./AccountGroup";
+
 
 
 const AccountType = axios.create({
@@ -19,3 +21,86 @@ export const GetAllAccountType = ()=>{
         },
     })
 }
+
+
+export const AddAccountType = (typeName) => {
+    const data = localStorage.getItem("auth")
+    const final = JSON.parse(data);
+
+    return AccountType({
+        method: "post",
+        url: "/AccountTypeService/api/AccountTypes/add",
+        data: {
+            "accountTypeName": `${typeName}`,
+            "lang": "fa",
+        },
+        headers: {
+            "selfuserid": `${final.userId}`,
+            "token": `${final.accessToken}`,
+            'Content-Type': 'application/json'
+        },
+    })
+}
+
+export const AccountTypeGetById = (id) => {
+    const data = localStorage.getItem("auth")
+    const final = JSON.parse(data);
+    return AccountType.get(`/AccountTypeService/api/AccountTypes/GetAccountType?AccountTypeId=${id}`, {
+        headers: {
+            "selfuserid": `${final.userId}`,
+            "token": `${final.accessToken}`
+        },
+    })
+}
+
+export const EditAccountType = (id, TypeId , isActive) => {
+    const data = localStorage.getItem("auth")
+    const final = JSON.parse(data);
+    return AccountType({
+        method: 'PUT',
+        url: '/AccountTypeService/api/AccountTypes/edit',
+        data: {
+            "accountTypeId": +id,
+            "accountTypeName": TypeId,
+            "isActive": isActive,
+            "lang": "fa",
+        },
+        headers: {
+            "selfuserid": `${final.userId}`,
+            "token": `${final.accessToken}`,
+            'Content-Type': 'application/json'
+        },
+    })
+}
+
+export const AccountTypeEditIsActive = (mainId, isActive) =>{
+    const data = localStorage.getItem("auth")
+    const final = JSON.parse(data);
+    return AccountType({
+        method: 'PUT',
+        url: '/AccountTypeService/api/AccountTypes/EditIsActive',
+        params: {
+            "AccountSpecId": mainId,
+            "IsActive": isActive
+        },
+        headers: {
+            "selfuserid": `${final.userId}`,
+            "token": `${final.accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+
+export const DeleteAccountType = (typeId) => {
+    const data = localStorage.getItem("auth")
+    const final = JSON.parse(data);
+    return AccountGroup.delete(`/AccountTypeService/api/AccountTypes/remove?AccountTypeId=${typeId}`, {
+        headers: {
+            "selfuserid": `${final.userId}`,
+            "token": `${final.accessToken}`
+        }
+    })
+}
+
+export default AccountType;
--- src/layout/AccountingType/AccountingType.jsx
+++ src/layout/AccountingType/AccountingType.jsx
@@ -1,18 +1,29 @@
 import {useEffect, useState} from "react";
-import {GetAllAccountType} from "../../api/AccountType";
+import {
+    AccountTypeGetById,
+    AddAccountType,
+    DeleteAccountType,
+    EditAccountType,
+    GetAllAccountType
+} from "../../api/AccountType";
 import {Alert, Breadcrumb, Button, Col, Container, Modal, Row} from "react-bootstrap";
 import {Link, useNavigate} from "react-router-dom";
 import Loader from "../../Loader/Loader";
 import FilterBox from "../../components/FilterBox/FilterBox";
 import ActionTableButton from "../../components/ActionTableButton/ActionTableButton";
 import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
-import {GetAllAccountSpec} from "../../api/AccountSpec";
+import {GetById} from "../../api/AccountGroup";
 
 const AccountingType = () => {
     const [account, setAccount] = useState(undefined);
     const [error, setError] = useState(false);
+    const [value, setValue] = useState({name: ""});
     const [show, setShow] = useState(false);
     const [message, setMessage] = useState("");
+    const [edit, setEdit] = useState({id: "",name: "", active: ""});
+    const [reload , setReload] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [editShow, setEditShow] = useState(false);
     const [errorShow, setErrorShow] = useState(false);
     const [successShow, setSuccessShow] = useState(false);
     const navigate = useNavigate();
@@ -20,10 +31,13 @@ const AccountingType = () => {
 
     const handleClose = () => {
         setShow(false);
-        // emptyInput()
     };
     const handleShow = () => setShow(true);
 
+
+    const manageChange = (e) => {
+        setValue({...value, [e.target.name]: e.target.value});
+    }
 
     const AccountTypeGetTabel = async () => {
         const data = await GetAllAccountType().catch(() => setError(true));
@@ -38,7 +52,99 @@ const AccountingType = () => {
 
     useEffect(()=>{
         AccountTypeGetTabel();
-    } , [])
+    } , [reload])
+
+
+    const manageAddAccount = async () => {
+        const addResponse = await AddAccountType(value.name);
+        console.log(addResponse)
+        if (addResponse.data.isSuccess === true) {
+            setMessage(addResponse.data.message);
+            setShow(false);
+            setSuccessShow(true);
+            emptyInput();
+            setReload(!reload);
+            setTimeout(() => {
+                setSuccessShow(false)
+            }, 2500)
+        } else {
+            setMessage(addResponse.data.message);
+            setShow(false);
+            setErrorShow(true);
+            setTimeout(() => {
+                setErrorShow(false)
+            }, 2500)
+        }
+
+    }
+
+    const manageRemoveAccount = async (id) => {
+        console.log(id)
+        const removeResponse = await DeleteAccountType(id);
+        console.log(removeResponse);
+        if (removeResponse.data.isSuccess === false) {
+            setMessage(removeResponse.data.message);
+            setErrorShow(true);
+            setTimeout(() => {
+                setErrorShow(false);
+                setMessage("");
+            }, 1000)
+        } else if (removeResponse.data.isSuccess === true) {
+            setMessage(removeResponse.data.message);
+            setSuccessShow(true);
+            setReload(!reload);
+            setTimeout(() => {
+                setSuccessShow(false);
+                setMessage("");
+            }, 1000)
+        }
+    }
+
+    const manageSendEditAccount = async () => {
+        const sendEditResponse = await EditAccountType(edit.id ,edit.name);
+        if (sendEditResponse.data.isSuccess === true) {
+            setLoading(!setReload(!reload))
+            setSuccessShow(true);
+            setEditShow(false);
+            setMessage(sendEditResponse.data.message);
+            setTimeout(() => {
+                setSuccessShow(false);
+            }, 2500)
+        } else {
+            setMessage(sendEditResponse.data.message);
+            setErrorShow(true);
+            setTimeout(() => {
+                setErrorShow(false);
+            }, 2500)
+        }
+    }
+
+    const manageEditChange = (e) => {
+        setEdit({...edit, [e.target.name]: e.target.value});
+    }
+
+    const handleEditClose = () => {
+        setEditShow(false);
+        emptyInput()
+    };
+
+    const manageEditAccount = async (id) => {
+        setEditShow(true);
+        setLoading(true)
+        const getResponse = await AccountTypeGetById(id);
+        console.log(getResponse)
+        setEdit({id:getResponse.data.accountTypeId , name:getResponse.data.accountTypeName , active:getResponse.data.isActive})
+        if (getResponse.status === 200) {
+            setLoading(false)
+        } else {
+            setEditShow(false)
+        }
+    }
+
+    const emptyInput = () => {
+        setValue({name: ""});
+    }
+
     return (
         <Container>
             <Row>
@@ -49,13 +155,8 @@ const AccountingType = () => {
                                 {'داشبورد'}
                             </Link>
                         </Breadcrumb.Item>
-                        <Breadcrumb.Item className={'beard_crumb'}>
-                            <Link to={'/accountingMain'}>
-                                {'حساب کل'}
-                            </Link>
-                        </Breadcrumb.Item>
                         <Breadcrumb.Item active>
-                            {'حساب معین'}
+                            {'نوع حساب'}
                         </Breadcrumb.Item>
                     </Breadcrumb>
                 </Col>
@@ -65,7 +166,7 @@ const AccountingType = () => {
                     <Row className={'mb-3'}>
                         <Col lg={1}>
                             <p className={'mt-1'}>
-                                {'حساب معین'}
+                                {'نوع حساب'}
                             </p>
                         </Col>
                         <Col>
@@ -76,8 +177,63 @@ const AccountingType = () => {
                         <Col>
                             <>
                                 <Button className={'btn_style'} onClick={handleShow}>
-                                    {"افزودن حساب معین"}
+                                    {"افزودن نوع حساب"}
                                 </Button>
+                                <Modal show={show} onHide={handleClose}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title className={'modal_title'}>
+                                            {"افزودن حساب کل"}
+                                        </Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body class={'d-flex flex-column justify-content-start p-3'}>
+                                        <Row className={"my-3"}>
+                                            <Col className={"d-flex align-items-center col-12"}>
+                                                <label style={{fontFamily: 'iran-sans'}}
+                                                       className={"me-2"}>{"نام حساب کل:"}</label>
+                                                <input name={"name"} onChange={manageChange} value={value.name}
+                                                       className={'p-2'}/>
+                                            </Col>
+                                        </Row>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button className={'close_btn'} onClick={handleClose}>
+                                            {"بستن"}
+                                        </Button>
+                                        <Button onClick={()=>manageAddAccount()}  className={'save_btn'}>
+                                            {"ایجاد حساب"}
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
+                                <Modal show={editShow} onHide={handleEditClose}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title className={'modal_title'}>
+                                            {"ویرایش حساب"}
+                                        </Modal.Title>
+                                    </Modal.Header>
+                                    {loading === true ?
+                                        <div className={"d-flex w-100 justify-content-center"}><Loader/>
+                                        </div> :
+                                        <Modal.Body
+                                            class={'d-flex flex-column justify-content-start p-3'}>
+                                            <Row className={"my-3"}>
+                                                <Col className={"d-flex align-items-center col-12"}>
+                                                    <label style={{fontFamily: 'iran-sans'}}
+                                                           className={"me-2"}>{"نام حساب:"}</label>
+                                                    <input name={"name"} onChange={manageEditChange}
+                                                           value={edit.name} className={'p-2'}/>
+                                                </Col>
+                                            </Row>
+                                        </Modal.Body>
+                                    }
+                                    <Modal.Footer>
+                                        <Button className={'close_btn'} onClick={handleEditClose}>
+                                            {"بستن"}
+                                        </Button>
+                                        <Button onClick={() => manageSendEditAccount()} className={'save_btn'}>
+                                            {"ویرایش گروه"}
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
                             </>
                         </Col>
                     </Row>
@@ -141,12 +297,14 @@ const AccountingType = () => {
                                                                        bgColor={"--color-warning"}
                                                                        tooltip={"ویرایش"}
                                                                        icon={faEdit}
+                                                                       onClick={() => manageEditAccount(item.accountTypeId)}
                                                     />
 
                                                     <ActionTableButton color={"--text-color-white"}
                                                                        bgColor={"--color-danger"}
-                                                                       tooltip={"حذف کاربر"}
+                                                                       tooltip={"حذف حساب"}
                                                                        icon={faTrash}
+                                                                       onClick={()=>manageRemoveAccount(item.accountTypeId)}
                                                     />
                                                 </td>
                                             </tr>
