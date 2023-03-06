499e505aedecad84acefdc6ee75856f3e33bc3cb
Aboufazel <a.ab2521@gmail.com>
2023-03-06 20:14:28 +0330

    feat : app layout and route done

--- src/layout/MobileLayout.jsx
+++ src/layout/MobileLayout.jsx
@@ -1,5 +1,6 @@
 import {Grid} from "@mui/material";
 import {Outlet} from "react-router-dom";
+import theme from "../themes/theme";
 
 const MobileLayout = ()=>{
 
@@ -12,6 +13,11 @@ const MobileLayout = ()=>{
                 item
                 display={"flex"}
                 justifyContent={"center"}
+                position={"relative"}
+                bgcolor={theme.palette.neutralN00.main}
+                mb={0}
+                overflow={"scroll"}
+                height={'97vh'}
                 maxWidth={500}
                 width={'100%'}
                 >
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -18,6 +18,8 @@ import LoadingPage from "../layout/Login/LoadingPage";
 import ForgetPass from "../layout/Auth/ForgetPass";
 import SignUp from "../layout/Auth/SignUp";
 import Verification from "../layout/Auth/Verification";
+import MobileLayout from "../layout/MobileLayout";
+import AccountingHome from "../layout/AppLayouts/AccountingHome/AccountingHome";
 
 const Root = () => {
     const router = createBrowserRouter(
@@ -25,107 +27,121 @@ const Root = () => {
             {
                 path: "/login",
                 element: <Login/>
-            } ,{
+            }, {
             path: "login/forgetPass",
             element: <ForgetPass/>
-        },{
+        }, {
             path: "login/verification",
             element: <Verification/>
-        },{
+        }, {
             path: "login/signUp",
             element: <SignUp/>
-        },
-            {
-                path: "/",
-                element: <IndexLayout/>,
-                children: [
-                    {
-                        path: "/",
-                        element: (
-                            <Suspense fallback={<LoadingPage/>}>
-                                <AuthProvider>
-                                    <Main/>
-                                </AuthProvider>
-                            </Suspense>
-                        )
-                    }, {
-                        path: '/allUser',
-                        element: (
-                            <Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <Category/>
-                                </AuthProvider>
-                            </Suspense>
-                        )
-                    }, {
-                        path: '/accountingGroup',
-                        element:
-                            (<Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <AccountingGroup/>
-                                </AuthProvider>
-                            </Suspense>)
-
-                    }, {
-                        path: '/accountingMain',
-                        element:
-                            (<Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <AccountingMain/>
-                                </AuthProvider>
-                            </Suspense>)
-
-                    }, {
-                        path: '/accountTotal',
-                        element:
-                            (<Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <AccountTotal/>
-                                </AuthProvider>
-                            </Suspense>)
-                    }, {
-                        path: '/accountType',
-                        element:
-                            (<Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <AccountingType/>
-                                </AuthProvider>
-                            </Suspense>)
-                    },{
-                        path: '/accountSpecType',
-                        element:
-                            (<Suspense fallback={<div>Loading...</div>}>
-                                <AuthProvider>
-                                    <AccountingSpecType/>
-                                </AuthProvider>
-                            </Suspense>)
-                    },{
-                    path:'/accountingDefaultPerson',
-                        element:(<Suspense fallback={<div>Loading...</div>}>
+        }, {
+            path: "/",
+            element: <IndexLayout/>,
+            children: [
+                {
+                    path: "/",
+                    element: (
+                        <Suspense fallback={<LoadingPage/>}>
                             <AuthProvider>
-                                <AccountingDefaultPerson/>
+                                <Main/>
+                            </AuthProvider>
+                        </Suspense>
+                    )
+                }, {
+                    path: '/allUser',
+                    element: (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <AuthProvider>
+                                <Category/>
+                            </AuthProvider>
+                        </Suspense>
+                    )
+                }, {
+                    path: '/accountingGroup',
+                    element:
+                        (<Suspense fallback={<div>Loading...</div>}>
+                            <AuthProvider>
+                                <AccountingGroup/>
                             </AuthProvider>
                         </Suspense>)
 
-                    },{
-                        path:'/personsLink',
-                        element:(<Suspense fallback={<div>Loading...</div>}>
+                }, {
+                    path: '/accountingMain',
+                    element:
+                        (<Suspense fallback={<div>Loading...</div>}>
                             <AuthProvider>
-                                <AccoutingPersonLink/>
+                                <AccountingMain/>
                             </AuthProvider>
                         </Suspense>)
 
-                    },
-                    {
-                        path: '/pass',
-                        element: <PageNotFound/>
-                    }, {
-                        path: '/editUser',
-                        element: <EditUser/>
-                    },
-                ]
+                }, {
+                    path: '/accountTotal',
+                    element:
+                        (<Suspense fallback={<div>Loading...</div>}>
+                            <AuthProvider>
+                                <AccountTotal/>
+                            </AuthProvider>
+                        </Suspense>)
+                }, {
+                    path: '/accountType',
+                    element:
+                        (<Suspense fallback={<div>Loading...</div>}>
+                            <AuthProvider>
+                                <AccountingType/>
+                            </AuthProvider>
+                        </Suspense>)
+                }, {
+                    path: '/accountSpecType',
+                    element:
+                        (<Suspense fallback={<div>Loading...</div>}>
+                            <AuthProvider>
+                                <AccountingSpecType/>
+                            </AuthProvider>
+                        </Suspense>)
+                }, {
+                    path: '/accountingDefaultPerson',
+                    element: (<Suspense fallback={<div>Loading...</div>}>
+                        <AuthProvider>
+                            <AccountingDefaultPerson/>
+                        </AuthProvider>
+                    </Suspense>)
 
-            }
+                }, {
+                    path: '/personsLink',
+                    element: (<Suspense fallback={<div>Loading...</div>}>
+                        <AuthProvider>
+                            <AccoutingPersonLink/>
+                        </AuthProvider>
+                    </Suspense>)
+
+                },
+                {
+                    path: '/pass',
+                    element: <PageNotFound/>
+                }, {
+                    path: '/editUser',
+                    element: <EditUser/>
+                },
+            ]
+
+        }, {
+            path: '/app',
+            element: <MobileLayout/>,
+            children: [
+                {
+                    path: "/app",
+                    element: (
+                        <Suspense fallback={<LoadingPage/>}>
+                            <AuthProvider>
+                                <AccountingHome/>
+                            </AuthProvider>
+                        </Suspense>
+                    )
+                }
+            ]
+        }
         ]
     )
     return (
